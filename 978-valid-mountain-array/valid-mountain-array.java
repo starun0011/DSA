e0a1fdc1397d890831d9b73046f67b9e27ac27dd
Tarun soni <starun6392@gmail.com>
2025-09-19 10:23:27 +0530

    Time: 2 ms (69.05%) | Memory: 45.4 MB (46.00%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3)
+            return false;
+
+        int peak = arr[1], l = arr[0];
+        int i = 1;
+        boolean isMount = false, isInc = arr[0] < arr[1] ? true : false, isDec = false; //1 2 3 4 2 5 2 0
+        while (i < arr.length) {
+            if (isInc) {
+                if (arr[i] > l) {
+                    peak = arr[i];
+                    l = arr[i];
+                    isInc = true;
+                } else {
+                    isInc = false;
+                }
+            }
+            if (isInc == false) {
+                if (arr[i] < peak && arr[i] < l) {
+                    l = arr[i];
+                    isMount = true;
+                } else {
+                    return false;
+                }
+            }
+            i++;
+        }
+
+        return isMount;
+
+    }
+}
