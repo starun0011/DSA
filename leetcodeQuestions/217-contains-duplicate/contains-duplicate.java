2284da0563bf493680660e59d0ab5d614b4a0764
Tarun soni <starun6392@gmail.com>
2026-05-13 18:30:40 +0530

    Time: 15 ms (64.25%) | Memory: 93.3 MB (58.88%) - LeetSync

--- leetcodeQuestions/217-contains-duplicate/contains-duplicate.java
+++ leetcodeQuestions/217-contains-duplicate/contains-duplicate.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         if(nums.length == 1) return false;
-        Arrays.sort(nums);
-        for(int i=1; i<nums.length; i++){
-            if(nums[i]==nums[i-1]){
+        HashSet seen = new HashSet<Integer>();
+        for(int i=0; i<nums.length;i++){
+            if(seen.contains(nums[i])){
                 return true;
+            }
+            else{
+                seen.add(nums[i]);
             }
         }
         return false;
