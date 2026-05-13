84fc71eac07acbd52988bc24ed624658ebd4bba3
Tarun soni <starun6392@gmail.com>
2026-05-13 19:18:04 +0530

    Time: 5 ms (62.07%) | Memory: 44.3 MB (82.37%) - LeetSync

--- leetcodeQuestions/242-valid-anagram/valid-anagram.java
+++ leetcodeQuestions/242-valid-anagram/valid-anagram.java
@@ -1,13 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-        if(Arrays.equals(sArray,tArray)){
-            return true;
+        int[] arr = new int[26];
+        for(int i=0; i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
         }
-        return false;
+        for(int val:arr){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
+        // Arrays.sort(sArray);
+        // Arrays.sort(tArray);
+        // if(Arrays.equals(sArray,tArray)){
+        //     return true;
+        // }
+        // return false;
     }
 }
