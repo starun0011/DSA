306819a7b707db6ac92442fe65741a1f71a4d7bc
Tarun soni <starun6392@gmail.com>
2026-05-16 11:53:00 +0530

    Time: 17 ms (25.74%) | Memory: 50 MB (29.21%) - LeetSync

--- leetcodeQuestions/49-group-anagrams/group-anagrams.java
+++ leetcodeQuestions/49-group-anagrams/group-anagrams.java
@@ -2,16 +2,37 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars); 
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+        for(String word : strs){
+            int[] count = new int[26];
+            for(int i=0; i<word.length(); i++){
+                count[word.charAt(i)-'a']++;
             }
+            StringBuilder sb = new StringBuilder();
+            for(int val : count){
+                sb.append(val);
+                sb.append('#');
+            }
+            String frequencyKey = sb.toString();
 
-            map.get(sortedWord).add(word);
+            if(!map.containsKey(frequencyKey)){
+                map.put(frequencyKey,new ArrayList<>());
+            }
+            map.get(frequencyKey).add(word);
         }
+
+
+
+
+        // for (String word : strs) {
+        //     char[] chars = word.toCharArray();
+        //     Arrays.sort(chars);
+        //     String sortedWord = new String(chars); 
+        //     if (!map.containsKey(sortedWord)) {
+        //         map.put(sortedWord, new ArrayList<>());
+        //     }
+
+        //     map.get(sortedWord).add(word);
+        // }
 
         return new ArrayList<>(map.values());
     }
